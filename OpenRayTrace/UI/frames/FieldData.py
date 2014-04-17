399ae6509e8ed9f856df63d7f7ea09945b71309e
Ben FrantzDale <benfrantzdale@gmail.com>
2014-04-17 08:56:38 -0400

    Added text boxes per http://www.java2s.com/Tutorial/Python/0380__wxPython/TextEntryExample.htm

--- OpenRayTrace/UI/frames/FieldData.py
+++ OpenRayTrace/UI/frames/FieldData.py
@@ -8,5 +8,14 @@ class FieldData(wx.MDIChildFrame):
                                   size=wx.Size(100,100),
                                   pos=wx.Point(100,100),
                                   style=wx.DEFAULT_FRAME_STYLE)
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        panel = wx.Panel(self, -1) 
+        basicLabel = wx.StaticText(panel, -1, "Basic Control:")
+        basicText = wx.TextCtrl(panel, -1, "I've entered some text!", size=(175, -1))
+        basicText.SetInsertionPoint(0)
+
+        pwdLabel = wx.StaticText(panel, -1, "Password:")
+        pwdText = wx.TextCtrl(panel, -1, "password", size=(175, -1),style=wx.TE_PASSWORD)
+        sizer = wx.FlexGridSizer(cols=2, hgap=6, vgap=6)
+        sizer.AddMany([basicLabel, basicText, pwdLabel, pwdText])
+        panel.SetSizer(sizer)
         self.Show(True)
