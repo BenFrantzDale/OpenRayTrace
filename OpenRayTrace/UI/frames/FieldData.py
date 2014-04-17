8532dc97b49f4e144a6d271a7b1277d82c895e61
Ben FrantzDale <benfrantzdale@gmail.com>
2014-04-17 08:55:03 -0400

    Now it's a reasonable size.

--- OpenRayTrace/UI/frames/FieldData.py
+++ OpenRayTrace/UI/frames/FieldData.py
@@ -2,10 +2,11 @@ import wx
 
 class FieldData(wx.MDIChildFrame):
     def __init__(self, parent):
-        wx.Frame.__init__(self, id=wx.NewId(), 
-                          parent=parent, 
-                          title='Field Data', 
-                          size=wx.Size(1,1),
-                          pos=wx.Point(100,100),
-                          style=wx.DEFAULT_FRAME_STYLE)
+        wx.MDIChildFrame.__init__(self, id=wx.NewId(), 
+                                  parent=parent, 
+                                  title='Field Data', 
+                                  size=wx.Size(100,100),
+                                  pos=wx.Point(100,100),
+                                  style=wx.DEFAULT_FRAME_STYLE)
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.Show(True)
