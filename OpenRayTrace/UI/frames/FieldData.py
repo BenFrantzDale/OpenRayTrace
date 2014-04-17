bc5ee29e21f8e327238ceed7f37e0ffbc6d3066d
Ben FrantzDale <benfrantzdale@gmail.com>
2014-04-17 09:08:23 -0400

    Have a table that looks reasonable.

--- OpenRayTrace/UI/frames/FieldData.py
+++ OpenRayTrace/UI/frames/FieldData.py
@@ -5,17 +5,23 @@ class FieldData(wx.MDIChildFrame):
         wx.MDIChildFrame.__init__(self, id=wx.NewId(), 
                                   parent=parent, 
                                   title='Field Data', 
-                                  size=wx.Size(100,100),
+                                  size=wx.Size(700,500),
                                   pos=wx.Point(100,100),
                                   style=wx.DEFAULT_FRAME_STYLE)
         panel = wx.Panel(self, -1) 
-        basicLabel = wx.StaticText(panel, -1, "Basic Control:")
-        basicText = wx.TextCtrl(panel, -1, "I've entered some text!", size=(175, -1))
-        basicText.SetInsertionPoint(0)
-
-        pwdLabel = wx.StaticText(panel, -1, "Password:")
-        pwdText = wx.TextCtrl(panel, -1, "password", size=(175, -1),style=wx.TE_PASSWORD)
-        sizer = wx.FlexGridSizer(cols=2, hgap=6, vgap=6)
-        sizer.AddMany([basicLabel, basicText, pwdLabel, pwdText])
+        nfields = 12
+        colLabels = ('field', 'x field', 'y field', 'weight')
+        defaults = ('', 0, 0, 1.0)
+        ncols = len(colLabels)
+        widgets = []
+        for label in colLabels:
+            widgets.append(wx.StaticText(panel, -1, label))
+        for rowi in range(nfields):
+            widgets.append(wx.CheckBox(panel, -1, str(rowi)))
+            for colj in range(1, ncols):
+                widgets.append(wx.TextCtrl(panel, -1, str(defaults[colj]), size=(175, -1)))
+                widgets[-1].SetInsertionPoint(0)
+        sizer = wx.FlexGridSizer(cols=ncols, hgap=6, vgap=6)
+        sizer.AddMany(widgets)
         panel.SetSizer(sizer)
         self.Show(True)
